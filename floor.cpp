e247820edf1b1959476d07ef6f4d1ef6c589b606
jyLee <lge2515@naver.com>
2020-11-24 11:05:45 +0900

    floor.cpp

--- floor.cpp
+++ floor.cpp
@@ -1,48 +1,24 @@
-#include <list>
+#include "floor.h"
 
-#include "hwatoo.cpp"
+void Floor::reset() { floor_list_.clear(); }
 
-class Floor {
- public:
-  Floor() {}
+void Floor::add_card(Hwatoo card) { floor_list_.push_back(card); }
 
-  void reset() { card_list.clear(); }
-
-  void add_card(Hwatoo card) { card_list.push_back(card); }
-
-  void remove_card(Hwatoo card) {
-    for (itor = card_list.begin(); itor != card_list.end(); ++itor) {
-      if ((*itor).is_same(card)) {
-        card_list.erase(itor++);
-        return;
-      } else {
-        itor++;
-      }
+void Floor::remove_card(Hwatoo card) {
+  for (itor = floor_list_.begin(); itor != floor_list_.end(); ++itor) {
+    if ((*itor).is_same(card)) {
+      floor_list_.erase(itor++);
+      return;
+    } else {
+      itor++;
     }
   }
+}
 
-  std::list<Hwatoo> get_card_list() { return this->card_list; }
-
-  void print() {
-    for (itor = card_list.begin(); itor != card_list.end(); ++itor) {
-      std::cout << (*itor).getName() << " " << std::endl;
-    }
+void Floor::print_card_list() {
+  for (itor = floor_list_.begin(); itor != floor_list_.end(); ++itor) {
+    std::cout << (*itor).getName() << " ";
   }
+  std::cout << std::endl;
+}
 
- private:
-  std::list<Hwatoo> card_list;
-  std::list<Hwatoo>::iterator itor;
-};
-
-/*int main() {
-  Floor floor;
-  floor.add_card(Hwatoo("1광"));
-  floor.add_card(Hwatoo("2쌍"));
-  floor.add_card(Hwatoo("jocker"));
-
-  floor.remove_card(Hwatoo("jocker"));
-  floor.print();
-
-  floor.reset();
-  floor.print();
-}*/
