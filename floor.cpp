edebf4f4095552f656e34e7a6a631b29b3733c53
jyLee <lge2515@naver.com>
2020-11-26 20:43:23 +0900

    floor.cpp

--- floor.cpp
+++ floor.cpp
@@ -15,6 +15,8 @@ void Floor::remove_card(Hwatoo card) {
   }
 }
 
+bool Floor::is_empty_floor_list() { return this->floor_list_.empty(); }
+
 void Floor::print_card_list() {
   for (itor = floor_list_.begin(); itor != floor_list_.end(); ++itor) {
     std::cout << (*itor).getName() << " ";
@@ -22,3 +24,20 @@ void Floor::print_card_list() {
   std::cout << std::endl;
 }
 
+// 매개변수로 받은 card와 floor_list에 같은 종류의 카드가 있다면 cnt를 증가하여 반환한다.
+int Floor::same_card_count(Hwatoo card) { 
+  int cnt = 0;
+  for (itor = floor_list_.begin(); itor != floor_list_.end(); ++itor) {
+    if ((*itor).is_same(card)) cnt++;
+  }
+  return cnt;
+}
+
+// 매개변수로 받은 card와 floor_list에 같은 종류의 카드가 있다면 변수 list에 저장하여 반환한다.
+std::list<Hwatoo> Floor::same_card_list(Hwatoo card) {
+  std::list<Hwatoo> list;
+  for (itor = floor_list_.begin(); itor != floor_list_.end(); ++itor) {
+    if ((*itor).is_same(card)) list.push_back(*itor);
+  }
+  return list;
+}
