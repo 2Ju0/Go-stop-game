e4aeac13f04378f014aacce05798ecc1f1a88ac4
jyLee <lge2515@naver.com>
2020-11-27 22:48:04 +0900

    result.h

---
+++ result.h
@@ -0,0 +1,15 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+#include <iostream>
+
+#include "player.h"
+
+// pay_point 는 실제 게임에서는 돈을 얼마 내야하는지인데
+// 프로그램에서는 포인트로 돈을 대체
+
+class Result {
+  void result(Player Winner, Player loser1, Player loser2);
+};
+
+#endif  // RESULT_H
