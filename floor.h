87433de75c3454eb2016129f9db68480833af3dc
jyLee <lge2515@naver.com>
2020-11-27 21:58:21 +0900

    floor.h

--- floor.h
+++ floor.h
@@ -11,24 +11,13 @@ class Floor {
   void reset();
   void add_card(Hwatoo card);
   void remove_card(Hwatoo card);
+  bool is_empty_floor_list();
   void print_card_list();
+  int same_card_count(Hwatoo card);
+  std::list<Hwatoo> same_card_list(Hwatoo card);
 
  private:
   std::list<Hwatoo> floor_list_;
   std::list<Hwatoo>::iterator itor;
 };
-
-/*int main() {
-  Floor floor;
-  floor.add_card(Hwatoo("1광"));
-  floor.add_card(Hwatoo("2쌍"));
-  floor.add_card(Hwatoo("jocker"));
-
-  floor.remove_card(Hwatoo("jocker"));
-  floor.print();
-
-  floor.reset();
-  floor.print();
-}*/
 #endif  // FLOOR_H
-
