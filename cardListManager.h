3f79db4218f6d8f1322b4f0dd3244ee6a305d5d8
woou4578 <woou4578@o.cnu.ac.kr>
2020-12-09 21:49:49 +0900

    cardListManager.h

--- cardListManager.h
+++ cardListManager.h
@@ -1,31 +1,31 @@
 #ifndef CARDLISTMANAGER_H
 #define CARDLISTMANAGER_H
 
-#include <list>
 #include <iostream>
+#include <vector>
+#include <string>
+
 #include "hwatoo.h"
 
 class CardListManager {
- public:
-  std::list<Hwatoo> get_g_list();
-  std::list<Hwatoo> get_m_list();
-  std::list<Hwatoo> get_d_list();
-  std::list<Hwatoo> get_p_list();
-  std::list<Hwatoo> get_list_type(Hwatoo card);
-  void add_card(Hwatoo* card);
-  Hwatoo* steal_card();
-  void remove_card(Hwatoo* card);
-  void print_g_list();
-  void print_m_list();
-  void print_d_list();
-  void print_p_list();
+public:
+  std::vector<Hwatoo> getGList();
+  std::vector<Hwatoo> getMList();
+  std::vector<Hwatoo> getDList();
+  std::vector<Hwatoo> getPList();
+  void eatCard(Hwatoo* card);
+  void removePCard();
+  Hwatoo* stealCard();
+  void printGList();
+  void printMList();
+  void printDList();
+  void printPList();
 
- private:
-  std::list<Hwatoo> g_list_;         
-  std::list<Hwatoo> m_list_;        
-  std::list<Hwatoo> d_list_;         
-  std::list<Hwatoo> p_list_;           
-  std::list<Hwatoo>::iterator itor;  
+private:
+  std::vector<Hwatoo> g_list_;
+  std::vector<Hwatoo> m_list_;
+  std::vector<Hwatoo> d_list_;
+  std::vector<Hwatoo> p_list_;
+  std::vector<Hwatoo>::iterator itor;
 };
 #endif  // CARDLISTMANAGER_H
-
