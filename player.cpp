d7d1c8edabc47d23410e61db4df864fa25f113f7
jyLee <lge2515@naver.com>
2020-12-09 22:56:28 +0900

    player.cpp

--- player.cpp
+++ player.cpp
@@ -2,152 +2,144 @@
 
 Player::Player(std::string name) : player_name_(name) {}  // Constructor
 
-void Player::reset() {  // player 멤버 변수 초기화
-  this->hand_list_.clear();
-  this->score_ = 0;
-  this->G_point_ = 0;
-  this->except_point_ = 0;
-  this->D_point_ = 0;
-  this->make_cheongdan_ = 0;
-  this->make_hongdan_ = 0;
-  this->make_chodan_ = 0;
-  this->M_point_ = 0;
-  this->make_godori_ = 0;
-  this->P_point_ = 0;
-  this->bbuk_count_ = 0;
-  this->shaking_count_ = 0;
+// 매개변수로 받은 card와 floor_list에 같은 종류의 카드가 있다면 변수 list에
+// 저장하여 반환한다.
+std::vector<Hwatoo> Player::sameCardList(Hwatoo card) {
+  std::vector<Hwatoo> list;
+  for (itor = hand_list_.begin(); itor != hand_list_.end(); ++itor) {
+    if ((*itor).isSame(card)) {
+      list.push_back(*itor);
+    }
+  }
+  return list;
 }
 
-void Player::set_name(std::string name) { this->player_name_ = name; }
+void Player::setName(std::string name) {
+  this->player_name_ = name;
+}  // player 이름 설정
 
-std::string Player::get_name() { return this->player_name_; }
+std::string Player::getName() {
+  return this->player_name_;
+}  // player 이름 반환
 
-void Player::add_card(Hwatoo card) {  // player의 손패 리스트에 카드 추가
+void Player::addCard(Hwatoo card) {  // player의 손패 리스트에 카드 추가
   hand_list_.push_back(card);
 }
 
-Hwatoo* Player::throw_card(Hwatoo card) {  // player의 손패 리스트에서 특정 카드
-                                           // 삭제, 삭제한 카드 주소 반환
-  for (itor = hand_list_.begin(); itor != hand_list_.end(); ++itor) {
-    if ((*itor).is_same(card)) {
-      Hwatoo removed_card = (*itor);
-      hand_list_.erase(itor++);
-      return &card;
+void Player::removeCard(
+    Hwatoo card) {  // player의 손패 리스트에서 특정 카드 삭제
+  for (itor = hand_list_.begin(); itor != hand_list_.end();) {
+    if ((*itor).getName() == card.getName()) {
+      hand_list_.erase(itor);
+      return;
     } else {
-      itor++;
+      ++itor;
     }
   }
-  return nullptr;  // 특정 카드가 없다면 nullptr 반환
 }
 
-int Player::same_card_count_in_hand_list(Hwatoo card) {
+int Player::sameCardCountInHandList(Hwatoo card) {
   int cnt = 0;
   for (itor = this->hand_list_.begin(); itor != this->hand_list_.end();
        ++itor) {
-    if ((*itor).is_same(card)) {
+    if ((*itor).isSame(card)) {
       cnt++;
     }
   }
   return cnt;
 }
 
-bool Player::is_empty_hand_list() { return this->hand_list_.empty(); }
+int Player::getGoCount() { return this->go_count_; }  // 몇 go인지 확인
 
-int Player::get_go_count() { return this->go_count_; }  // 몇 go인지 확인
+void Player::plusGoCount() { this->go_count_++; }  // go 값 증가
 
-void Player::plus_go_count() { this->go_count_++; }  // go 값 증가
-
-void Player::print_hand_list() {  // player의 카드 리스트 출력
+void Player::printHandList() {  // player의 카드 리스트 출력
+  int number = 0;
   for (itor = hand_list_.begin(); itor != hand_list_.end(); ++itor) {
-    std::cout << (*itor).getName() << " ";
+    std::cout << "[" << number++ << "]" << (*itor).getMonth() << (*itor).getKind() << " ";
   }
   std::cout << std::endl;
 }
 
-std::list<Hwatoo> Player::get_hand_list() { return this->hand_list_; }
+std::vector<Hwatoo> Player::getHandList() {
+  return this->hand_list_;
+}  // player의 손패 리스트 반환
 
-int Player::get_total_score() { return this->score_; }  // player 점수 반환
+void Player::setTempScore(int score) { this->temp_score_ = score; }
 
-int Player::get_bbuk_count() { return this->bbuk_count_; }
+int Player::getTempScore() { return this->temp_score_; }
 
-void Player::plus_bbuk_count() { this->bbuk_count_++; }
+int Player::getTotalScore() { return this->score_; }  // player 점수 반환
 
-int Player::get_shaking_count() { return this->shaking_count_; }
+int Player::getBbukCount() { return this->bbuk_count_; }  // 뻑 횟수 반환
 
-void Player::reset_shaking_count() { this->shaking_count_ = 0; }
+void Player::plusBbukCount() { this->bbuk_count_++; }  // 뻑 횟수 증가
 
-void Player::plus_shaking_count() { this->shaking_count_++; }
+int Player::getShakingCount() {
+  return this->shaking_count_;
+}  // 흔들기 횟수 반환
 
-void Player::is_winner(bool winner) { this->winner = winner; }
+void Player::resetShakingCount() {
+  this->shaking_count_ = 0;
+}  // 흔들기 횟수 초기화
 
-void Player::cal_G() {
-  std::list<Hwatoo> list = this->my_card_list_.get_g_list();
+void Player::plusShakingCount() {
+  this->shaking_count_++;
+}  // 흔들기 횟수 증가
+
+void Player::calG() {  // 광 계산
+  this->G_point_ = 0;
+  this->except_point_ = 0;
+  std::vector<Hwatoo> list = this->my_card_list_.getGList();
+
+  // 광 개수 증가시키는 부분
   for (itor = list.begin(); itor != list.end(); ++itor) {
-    char month = (*itor).getMonth();
-    if (month == '1') {
-      this->G_point_++;
-    } else if (month == '3') {
-      this->G_point_++;
-    } else if (month == '8') {
-      this->G_point_++;
-    } else if (month == 'D') {
-      this->G_point_++;
-    } else {
-      this->except_point_;
-    }
+    int month = (*itor).getMonth();
+    if (month == 12) {  // 비광 개수 증가
+      this->except_point_++;
+    } 
   }
-
-  if (this->G_point_ == 2 && this->except_point_ == 1) {
-    this->score_ += 2;
-    //비광 1개와 다른 종류의 광2개 먹은 경우
-  } else if (this->G_point_ == 4 && this->except_point_ == 1) {
+  this->G_point_ = list.size();
+  if (this->G_point_ == 5) {  // 5광
     this->score_ += 15;
-  } else if (this->G_point_ >= 3) {
-    this->score_ = G_point_ + except_point_;
+  } else if (this->G_point_ == 4) {  // 4광
+    this->score_ += 4;
+  } else if (this->G_point_ == 3) {  // 3광
+    if (this->except_point_ == 1) {  // 비광포함
+      this->score_ += 2;
+    } else {
+      this->score_ += 3;
+    }
   }
 }
 
-void Player::cal_D() {
-  std::list<Hwatoo> list = this->my_card_list_.get_d_list();
+void Player::calD() {  // 띠 계산
+  this->make_cheongdan_ = 0;
+  this->make_chodan_ = 0;
+  this->make_hongdan_ = 0;
+  this->D_point_ = 0;
+  std::vector<Hwatoo> list = this->my_card_list_.getDList();
+
   for (itor = list.begin(); itor != list.end(); ++itor) {
     // 청단 확인
-    char month = (*itor).getMonth();
-    if (month == '6') {
+    int month = (*itor).getMonth();
+    if (month == 6 || month == 9 || month == 10) {
       this->make_cheongdan_++;
-      continue;
-    } else if (month == '9') {
-      this->make_cheongdan_++;
-      continue;
-    } else if (month == 'P') {
-      this->make_cheongdan_++;
-      continue;
     }
     // 홍단 확인
-    if (month == '1') {
+    if (month == 1 || month == 2 || month == 3) {
       this->make_hongdan_++;
-      continue;
-    } else if (month == '2') {
-      this->make_hongdan_++;
-      continue;
-    } else if (month == '3') {
-      this->make_hongdan_++;
-      continue;
     }
-
     // 초단 확인
-    if (month == '4') {
+    if (month == 4 || month == 5 || month == 7) {
       this->make_chodan_++;
-      continue;
-    } else if (month == '5') {
-      this->make_chodan_++;
-      continue;
-    } else if (month == '7') {
-      this->make_chodan_++;
-      continue;
     }
   }
 
   D_point_ = list.size();
+  if (this->D_point_ >= 5) {  // 띠가 5장 이상인 경우
+    this->score_ += (this->D_point_ - 4);
+  }
   if (this->make_cheongdan_ == 3) {
     this->score_ += 3;
   }
@@ -157,34 +149,32 @@ void Player::cal_D() {
   if (this->make_chodan_ == 3) {
     this->score_ += 3;
   }
-  if (this->D_point_ >= 5) {  // 띠가 5장 이상인 경우
-    this->score_ += (this->D_point_ - 4);
-  }
 }
 
-void Player::cal_M() {
-  std::list<Hwatoo> list = this->my_card_list_.get_m_list();
+void Player::calM() {  // 멍 계산
+  this->M_point_ = 0;
+  this->make_godori_ = 0;
+  std::vector<Hwatoo> list = this->my_card_list_.getMList();
+
   for (itor = list.begin(); itor != list.end(); ++itor) {
-    char month = (*itor).getMonth();
-    if (month == '2') {
-      this->make_godori_++;
-    } else if (month == '4') {
-      this->make_godori_++;
-    } else if (month == '8') {
+    int month = (*itor).getMonth();
+    if (month == 2 || month == 4 || month == 8) {
       this->make_godori_++;
     }
   }
-  M_point_ = list.size();
   if (this->make_godori_ == 3) {  // 고도리 계산
     this->score_ += 5;
   }
+  M_point_ = list.size();
   if (this->M_point_ >= 5) {  // 멍이 5개 이상인 경우
     this->score_ += (this->M_point_ - 4);
   }
 }
 
-void Player::cal_P() {
-  std::list<Hwatoo> list = this->my_card_list_.get_p_list();
+void Player::calP() {  // 피 계산
+  this->P_point_ = 0;
+  std::vector<Hwatoo> list = this->my_card_list_.getPList();
+
   for (itor = list.begin(); itor != list.end(); ++itor) {
     std::string kind = (*itor).getKind();
     if (kind == "쌍") {
@@ -192,17 +182,17 @@ void Player::cal_P() {
     } else {
       this->P_point_ += 1;
     }
-    if (this->P_point_ >= 10) {  // 피가 10개 이상인 경우
-      this->score_ += (this->P_point_ - 9);
-    }
+  }
+
+  if (this->P_point_ >= 10) {  // 피가 10개 이상인 경우
+    this->score_ += (this->P_point_ - 9);
   }
 }
 
-void Player::cal_total_score() {
+void Player::calTotalScore() {  // 최종 점수 계산
   this->score_ = 0;
-  this->cal_G();
-  this->cal_M();
-  this->cal_D();
-  this->cal_P();
+  this->calG();
+  this->calM();
+  this->calD();
+  this->calP();
 }
-
