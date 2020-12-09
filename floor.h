08e2086402fd8cc03091258af299e25f5b779548
woou4578 <woou4578@o.cnu.ac.kr>
2020-12-09 21:51:03 +0900

    floor.h

--- floor.h
+++ floor.h
@@ -1,23 +1,22 @@
 #ifndef FLOOR_H
 #define FLOOR_H
 
-#include <list>
 #include <iostream>
+#include <vector>
 
 #include "hwatoo.h"
 
 class Floor {
- public:
-  void reset();
-  void add_card(Hwatoo card);
-  void remove_card(Hwatoo card);
-  bool is_empty_floor_list();
-  void print_card_list();
-  int same_card_count(Hwatoo card);
-  std::list<Hwatoo> same_card_list(Hwatoo card);
+public:
+  void addCard(Hwatoo card);
+  Hwatoo* removeCard(Hwatoo card);
+  bool isEmptyFloorList();
+  void printCardList();
+  int sameCardCount(Hwatoo card);
+  std::vector<Hwatoo> sameCardList(Hwatoo card);
 
- private:
-  std::list<Hwatoo> floor_list_;
-  std::list<Hwatoo>::iterator itor;
+private:
+  std::vector<Hwatoo> floor_list_;
+  std::vector<Hwatoo>::iterator itor;
 };
 #endif  // FLOOR_H
