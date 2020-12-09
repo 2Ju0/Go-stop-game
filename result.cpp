5ec2293c3c4fa3cd7fca91dee429d9f94bc5bbfd
jyLee <lge2515@naver.com>
2020-12-09 22:57:39 +0900

    result.cpp

--- result.cpp
+++ result.cpp
@@ -1,65 +1,69 @@
-#include <iostream>
-
 #include "result.h"
 
- void Result::result(Player Winner, Player loser1, Player loser2) {
+void Result::result(Player* Winner, Player* loser1, Player* loser2) {
   // 매개변수로 승자, 패자, 패자 순서대로 입력
-  // 박 확인하기
-  int pay_point_1 = Winner.get_total_score();
-  int pay_point_2 = Winner.get_total_score();
+  int winner_score = (*Winner).getTotalScore() + (*Winner).getGoCount();
+  int pay_point_1 = winner_score;
+  int pay_point_2 = winner_score;
 
   // 폭탄 점수 처리
-  if (Winner.get_shaking_count() == 2) {
+  if ((*Winner).getShakingCount() == 2) {
     pay_point_1 *= 4;
     pay_point_2 *= 4;
-  } else if (Winner.get_shaking_count() == 1) {
+  } else if ((*Winner).getShakingCount() == 1) {
     pay_point_1 *= 2;
     pay_point_2 *= 2;
   }
   // 3고 이상시 점수처리
-  if (Winner.get_go_count() >= 4) {  // 4고 이상 4배 처리
+  if ((*Winner).getGoCount() >= 4) {  // 4고 이상 4배 처리
     pay_point_1 *= 4;
     pay_point_2 *= 4;
-  } else if (Winner.get_go_count() == 3) {  // 3고 2배 처리
+  } else if ((*Winner).getGoCount() == 3) {  // 3고 2배 처리
     pay_point_1 *= 2;
     pay_point_2 *= 2;
   }
   // 광박 계산
-  if (Winner.my_card_list_.get_g_list().size() >= 3 &&
-      loser1.my_card_list_.get_g_list().size() == 0) {
+  if ((*Winner).my_card_list_.getGList().size() >= 3 &&
+      (*loser1).my_card_list_.getGList().size() == 0) {
     pay_point_1 *= 2;
   }
-  if (Winner.my_card_list_.get_g_list().size() >= 3 &&
-      loser2.my_card_list_.get_g_list().size() == 0) {
+  if ((*Winner).my_card_list_.getGList().size() >= 3 &&
+      (*loser2).my_card_list_.getGList().size() == 0) {
     pay_point_2 *= 2;
   }
-  // 피박 계산
-  if (Winner.my_card_list_.get_p_list().size() >= 10 &&
-      loser1.my_card_list_.get_p_list().size() < 6) {
+  // 피박 계산 >> 룰 예외 처리 필요
+  if ((*Winner).my_card_list_.getPList().size() >= 10 &&
+      (*loser1).my_card_list_.getPList().size() < 6) {
     pay_point_1 *= 2;
   }
-  if (Winner.my_card_list_.get_p_list().size() >= 10 &&
-      loser2.my_card_list_.get_p_list().size() < 6) {
+  if ((*Winner).my_card_list_.getPList().size() >= 10 &&
+      (*loser2).my_card_list_.getPList().size() < 6) {
     pay_point_2 *= 2;
   }
   // 멍박 계산
-  if (Winner.my_card_list_.get_m_list().size() >= 7) {
+  if ((*Winner).my_card_list_.getMList().size() >= 7) {
     pay_point_1 *= 2;
     pay_point_2 *= 2;
   }
-  // 나가리 된 경우 계산
-  /*if ( 게임이 리셋 되는 경우 (전판이 나가리 된 경우) ) {
-    pay_point_1 *= 2;
-    pay_point_2 *= 2;
-  }*/
+  // 재경기 인 경우 계산
+  if (number_of_game_ > 0) {
+    pay_point_1 *= (2 * number_of_game_);
+    pay_point_2 *= (2 * number_of_game_);
+  }
   // 고박 계산
-  // 상대가 1고 이상을 했지만 다른 사람이 역전을 해서 고를 한 사람이 진 경우
-  // 그 사람이 패자들의 점수 모두 계산
-  if (loser1.get_go_count() != 0) {
+  if ((*loser1).getGoCount() != 0) {
     pay_point_1 += pay_point_2;
     pay_point_2 = 0;
-  } else if (loser2.get_go_count() != 0) {
+  } else if ((*loser2).getGoCount() != 0) {
     pay_point_2 += pay_point_1;
     pay_point_1 = 0;
   }
+  std::cout << std::endl;
+  std::cout << " [" << Winner->getName() << "]가 총 " << winner_score
+            << " 점을 획득했습니다." << std::endl;
+  std::cout << " [" << loser1->getName() << "]가 " << pay_point_1
+            << " point를 잃습니다." << std::endl;
+  std::cout << " [" << loser2->getName() << "]가 " << pay_point_2
+            << " point를 잃습니다." << std::endl;
 }
+
