3aedf172c184d3853b6ada771955fc7233a361d2
woou4578 <woou4578@o.cnu.ac.kr>
2020-12-09 21:52:05 +0900

    player.h

--- player.h
+++ player.h
@@ -1,40 +1,44 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <string>
-#include <list>
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <string>
+
 #include "cardListManager.h"
 
 class Player {
- public:
+public:
   explicit Player(std::string name);
   CardListManager my_card_list_ = CardListManager();
-  void reset();
-  void set_name(std::string name);
-  std::string get_name();
-  void add_card(Hwatoo card);
-  Hwatoo* throw_card(Hwatoo card);
-  int same_card_count_in_hand_list(Hwatoo card);
-  bool is_empty_hand_list();
-  int get_go_count();
-  void plus_go_count();
-  int get_total_score();
-  void print_hand_list();
-  void cal_total_score();
+  void setName(std::string name);
+  std::string getName();
+  void addCard(Hwatoo card);
+  void removeCard(Hwatoo card);
+  int sameCardCountInHandList(Hwatoo card);
+  std::vector<Hwatoo> sameCardList(Hwatoo card);
+  int getGoCount();
+  void plusGoCount();
+  int getTotalScore();
+  void printHandList();
+  std::vector<Hwatoo> getHandList();
+  void setTempScore(int score);
+  int getTempScore();
+  void calTotalScore();
+  int getBbukCount();
+  void plusBbukCount();
+  int getShakingCount();
+  void plusShakingCount();
+  bool winner_ = false;
 
-  int get_bbuk_count(); // ¿¿¿ ¿¿... ¿ ¿¿¿¿ 3¿¿ ¿¿ ¿
-  int get_bomb_count(); // ¿¿¿ ¿¿
-
- private:
-  std::list<Hwatoo> hand_list_;
-  std::list<Hwatoo>::iterator itor;
+private:
+  std::vector<Hwatoo> hand_list_;
+  std::vector<Hwatoo>::iterator itor;
   std::string player_name_;
   int go_count_ = 0;
-  bool winner = false; // winner¿ true¿ ¿¿ ¿¿ ¿!
 
-  // score ¿¿
+  // score °ü·Ã
+  int temp_score_ = 0;
   int score_ = 0;
   int G_point_ = 0;
   int except_point_ = 0;
@@ -46,29 +50,11 @@ class Player {
   int make_godori_ = 0;
   int P_point_ = 0;
   int bbuk_count_ = 0;
-  int bomb_count_ = 0; // ¿¿¿ ¿¿bock¿¿ ¿¿¿¿ ¿¿
-
-  // ¿¿¿ ¿¿
-  // ¿¿ ¿ ¿¿¿¿
-  int check_1 = 0;
-  int check_2 = 0;
-  int check_3 = 0;
-  int check_4 = 0;
-  int check_5 = 0;
-  int check_6 = 0;
-  int check_7 = 0;
-  int check_8 = 0;
-  int check_9 = 0;
-  int check_10 = 0;
-  int check_11 = 0;
-  int check_12 = 0;
-
-  void cal_G();
-  void cal_D();
-  void cal_M();
-  void cal_P();
-  void check_bomb(); // ¿¿¿ ¿¿
-  void check_chongtong(); // ¿¿¿ ¿¿
+  int shaking_count_ = 0;
+  void calG();
+  void calD();
+  void calM();
+  void calP();
 };
 
 #endif  // PLAYER_H
