8943162b3f947c31bb9240305846557cf6f6df48
JuYeong-Lee <67141218+JuYeong-Lee@users.noreply.github.com>
2020-12-12 22:08:18 +0900

    board.h

--- board.h
+++ board.h
@@ -1,17 +1,1 @@
-#ifndef BOARD_H
-#define BOARD_H
 
-#include <iostream>
-#include <string>
-
-#include "deck.h"
-
-class Board {
- public:
-  void gameStart(Deck* deck);
-  void gameOver();
-  void printPlayer(Player player);
-  void printFloor(Floor floor);
-  void line();
-};
-#endif  // GOSTOP_BOARD_H_
